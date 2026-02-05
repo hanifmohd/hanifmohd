86a90546b0ccaf1870d3ad9d13fbdaeed65ef83a
Hanifmohd <hanifmohd84@gmail.com>
2026-02-06 05:42:39 +0800

    updat baru

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 
-$recepient = "your.mail@here.com";
-$sitename = "Arter";
+$recepient = "hanifmohd84@gmail.com";
+$sitename = "Hanifmohd";
 
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
